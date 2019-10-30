82f9fe0e329c5cdbf456cb4c461c16b850461b13
victor manuel preciado <victorpreciado004@gmail.com>
2019-10-30 11:24:36 -0500

    Subiendo Proyecto

---
+++ src/main/java/votaciones/Logica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package votaciones;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author victor
+ */
+@Named(value = "logica")
+@Dependent
+public class Logica {
+
+    /**
+     * Creates a new instance of Logica
+     */
+    public Logica() {
+    }
+    
+}
